9f20902e15e2168633c08cb6016ae292e3baa61d
Tomer Doitshman <>
2023-04-21 16:05:02 +0300

    ass1 proj first commit

--- assignment1/my_id3.py
+++ assignment1/my_id3.py
@@ -3,15 +3,20 @@ from sklearn.base import BaseEstimator, ClassifierMixin
 from sklearn.utils.validation import check_X_y, check_array, check_is_fitted
 from sklearn.utils.multiclass import unique_labels
 import warnings
+from sklearn.svm import LinearSVC
 from sklearn.preprocessing import LabelEncoder
 from entropy_calculator import EntropyCalculator
 from data_handler import DataHandler
+from sklearn.utils.estimator_checks import check_estimator
 warnings.filterwarnings("ignore")
 
 
 class MyID3(BaseEstimator, ClassifierMixin):
     def __init__(self, max_depth=3):
         self.max_depth = max_depth
+
+    def _more_tags(self):
+        return {'binary_only': True, 'multioutput': False, 'poor_score': True}
 
     def convert_labels(self, y):
         y = check_array(y, ensure_2d=False, dtype=None)
@@ -23,110 +28,161 @@ class MyID3(BaseEstimator, ClassifierMixin):
         return y
 
     def fit(self, X, y):
-        # Check that X and y have correct shape
-        X, y = check_X_y(X, y)
-        y = self.convert_labels(y)
-        # Store the classes seen during fit
-        self.classes_ = unique_labels(y)
-        self.X_ = X
-        self.y_ = y
-        self.tree_ = self.build_tree(X, y, depth=0, node_indices=np.arange(len(X)))  # build the decision tree
-        # Return the classifier
-        return self
+        try:
+            X, y = check_X_y(X, y)
+            # Check that X and y have correct shape
+            if len(X) != len(y):
+                raise Exception("X and y length are not compatible.")
+            if not len(X):
+                raise ValueError("Empty data rows X.")
+            if not X.shape[1]:
+                raise ValueError("0 feature(s) (shape=(%d, 0)) while a minimum of 2 is required." % X.shape[0])
+            if len(np.unique(y)) == 1:
+                raise ValueError("Classifier can't train when only one class is present.")
+            y = self.convert_labels(y)
+            # Store the classes seen during fit
+            self.classes_ = unique_labels(y)
+            self.classes_to_binary_ = {self.classes_[0]: 0, self.classes_[1]: 1}
+            self.n_features_in_ = X.shape[1]
+            self.tree_ = self.build_tree(X, y, depth=0, node_indices=np.arange(len(X)))  # build the decision tree
+            # Return the classifier
+            return self
+        except Exception as e:
+            print('Exception %s occurred during fit.' % e)
+            raise e
 
     def build_tree(self, X, y, depth, node_indices):
-        """
-        Recursive function to build the decision tree.
+        try:
+            """
+            Recursive function to build the decision tree.
+    
+            Args:
+                X (ndarray): Data matrix of shape (n_samples, n_features).
+                y (ndarray): Data matrix of shape (n_samples, ).
+                depth (int): Current depth of the decision tree.
+    
+            Returns:
+                tree (dict): A dictionary representing the decision tree.
+            """
+            entropy_calc = EntropyCalculator()
+            data_handler = DataHandler()
+            # Check if max depth is reached or if all labels are the same
+            if depth == self.max_depth or np.unique(y).shape[0] == 1:
+                return {"leaf": True, "class": np.argmax(np.bincount(y[node_indices])), 'instances': node_indices}
 
-        Args:
-            X (ndarray): Data matrix of shape (n_samples, n_features).
-            y (ndarray): Data matrix of shape (n_samples, ).
-            depth (int): Current depth of the decision tree.
+            # Find the best split
+            best_feature = data_handler.get_best_split(X, y, node_indices)
 
-        Returns:
-            tree (dict): A dictionary representing the decision tree.
-        """
-        entropy_calc = EntropyCalculator()
-        data_handler = DataHandler()
-        # Check if max depth is reached or if all labels are the same
-        if depth == self.max_depth or np.unique(y).shape[0] == 1:
-            return {"leaf": True, "class": np.argmax(np.bincount(y)), 'instances': node_indices}
+            # If no split found, return a leaf node with the majority class
+            if best_feature is None:
+                return {"leaf": True, "class": np.argmax(np.bincount(y[node_indices])), 'instances': node_indices}
 
-        # Find the best split
-        best_feature = data_handler.get_best_split(X, y, node_indices)
+            # Split the dataset
+            left_indices, right_indices = entropy_calc.split_dataset(X, node_indices, best_feature)
+            left_X, left_y = X, y
+            right_X, right_y = X, y
 
-        # If no split found, return a leaf node with the majority class
-        if best_feature is None:
-            return {"leaf": True, "class": np.argmax(np.bincount(y)), 'instances': node_indices}
+            # Recursively build left and right subtrees
+            left_tree = self.build_tree(left_X, left_y, depth + 1, left_indices)
+            right_tree = self.build_tree(right_X, right_y, depth + 1, right_indices)
 
-        # Split the dataset
-        left_indices, right_indices = entropy_calc.split_dataset(X, node_indices, best_feature)
-        left_X, left_y = X[left_indices], y[left_indices]
-        right_X, right_y = X[right_indices], y[right_indices]
-
-        # Recursively build left and right subtrees
-        left_tree = self.build_tree(left_X, left_y, depth + 1, left_indices)
-        right_tree = self.build_tree(right_X, right_y, depth + 1, right_indices)
-
-        # Create a dictionary to represent the current node
-        node = {"leaf": False, "feature": best_feature, "left": left_tree, "right": right_tree,
-                'instances': node_indices}
-        return node
+            # Create a dictionary to represent the current node
+            node = {"leaf": False, "feature": best_feature, "left": left_tree, "right": right_tree,
+                    'instances': node_indices}
+            return node
+        except Exception as e:
+            print('Exception %s occurred during build_tree.' % e)
+            raise e
 
     def predict_proba(self, X):
-        """
-        Predict class probabilities for input data using a trained model.
-
-        Args:
-            X (ndarray): Data matrix of shape (n_samples, n_features).
-
-        Returns:
-            Prob (ndarray): Probability matrix of shape (n_samples, n_classes).
-        """
-        # Return probability matrix
-        Prob = np.zeros((X.shape[0], len(self.classes_)))
-        for i, sample in enumerate(X):
-            node = self.tree_
-            while not node["leaf"]:
-                if sample[node["feature"]] == 0:
-                    node = node["left"]
-                else:
-                    node = node["right"]
-            Prob[i, node["class"]] = 1
-        return Prob
+        try:
+            """
+            Predict class probabilities for input data using a trained model.
+    
+            Args:
+                X (ndarray): Data matrix of shape (n_samples, n_features).
+    
+            Returns:
+                Prob (ndarray): Probability matrix of shape (n_samples, n_classes).
+            """
+            # Return probability matrix
+            Prob = np.zeros((X.shape[0], len(self.classes_)))
+            for i, sample in enumerate(X):
+                node = self.tree_
+                while not node["leaf"]:
+                    if sample[node["feature"]] == 0:
+                        node = node["left"]
+                    else:
+                        node = node["right"]
+                Prob[i, self.classes_to_binary_[node["class"]]] = 1
+            return Prob
+        except Exception as e:
+            print('Exception %s occurred during predict_proba.' % e)
+            raise e
 
     def predict(self, X):
-        # Check if fit has been called
-        check_is_fitted(self)
-        X = check_array(X)
-        # Convert input to numpy array
-        X = np.array(X)
+        try:
+            if len(np.unique(self.classes_)) == 1:
+                raise ValueError("Classifier can't predict when only one class is present.")
+            X = check_array(X)
+            # Check if fit has been called
+            if X.shape[1] != self.n_features_in_:
+                raise AssertionError("Number of in features in train is different from number in predict.")
+            check_is_fitted(self)
+            # Convert input to numpy array
+            X = np.array(X)
 
-        # Handle the case where there is only one class in the training data
-        if len(self.classes_) == 1:
-            return np.array([self.classes_[0]] * len(X))
+            # Handle the case where there is only one class in the training data
+            if len(self.classes_) == 1:
+                return np.array([self.classes_[0]] * len(X))
 
-        # Make predictions using the decision tree
-        y_pred = np.array([self._predict_instance(x, self.tree_) for x in X])
+            # Make predictions using the decision tree
+            y_pred = np.array([self._predict_instance(x, self.tree_) for x in X])
 
-        # Convert predicted labels to the original class labels
-        label_encoder = LabelEncoder()
-        y_pred = label_encoder.fit_transform(y_pred)
+            # Convert predicted labels to the original class labels
+            label_encoder = LabelEncoder()
+            y_pred = label_encoder.fit_transform(y_pred)
 
-        return y_pred
+            return y_pred
+        except Exception as e:
+            print('Exception %s occurred during predict.' % e)
+            raise e
 
     def _predict_instance(self, x, node):
-        if node["leaf"]:
-            return node["class"]
-        else:
-            feature = node["feature"]
-            if x[feature] == 0:
-                return self._predict_instance(x, node["left"])
+        try:
+            if node["leaf"]:
+                return node["class"]
             else:
-                return self._predict_instance(x, node["right"])
+                feature = node["feature"]
+                if x[feature] == 0:
+                    return self._predict_instance(x, node["left"])
+                else:
+                    return self._predict_instance(x, node["right"])
+        except Exception as e:
+            print('Exception %s occurred during _predict_instance.' % e)
+            raise e
+
+
+def model_check():
+    # check_estimator(LinearSVC())  # passes
+    test_gen = check_estimator(MyID3(), True)  # passes
+    tests_to_run = []
+    tests_to_skip = [16, 18, 19, 20, 21, 24, 40]  # 1, 12 works, 16-multiclass-skip, 18-multiclass-skip
+    # , 19-multiclass-skip, 20-multiclass-skip, 21-regression-skip, 24-regression-skip, 29-works, 40-multiclass-skip
+    # 5-complex-num-skip, 6-multiclass-skip, 7-works, 9-works, 11-works, 14-works, 15-works
+    for i, t in enumerate(test_gen):
+        try:
+            if i in tests_to_skip:
+                continue
+            print('Running test %d.' % i)
+            t[1](t[0])
+            print('test %d passed successfully.' % i)
+        except AssertionError as e:
+            print('Test %d failed with %s.' % (i, e))
 
 
 if __name__ == '__main__':
+    model_check()
     # Create a sample dataset
     X = np.array([
         [0, 1, 1],
@@ -139,7 +195,7 @@ if __name__ == '__main__':
     y = np.array([0, 1, 0, 1, 0, 1])
 
     # Initialize the MyID3 classifier
-    clf = MyID3(max_depth=2)
+    clf = MyID3(max_depth=3)
 
     # Train the classifier on the sample data
     clf.fit(X, y)
