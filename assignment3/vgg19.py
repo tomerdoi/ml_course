97d55c01fcd3ea4a11517b0421931e6eec17fe71
Tomer Doitshman <>
2023-05-29 23:52:36 +0300

    ass1 proj first commit

--- assignment3/vgg19.py
+++ assignment3/vgg19.py
@@ -1,2 +1,61 @@
 from vgg_pytorch import VGG
 model = VGG.from_pretrained('vgg19', num_classes=10)
+
+import torch
+import torchvision.transforms as transforms
+from torchvision.datasets import ImageFolder
+from torch.utils.data import DataLoader
+from vgg_pytorch import VGG
+
+
+num_epochs = 1000
+# Set the path to the train and test folders
+train_folder = 'path/to/train/folder'
+test_folder = 'path/to/test/folder'
+
+# Define the transformation to apply to the images
+transform = transforms.Compose([
+    transforms.Resize((224, 224)),  # Resize the images to VGG input size
+    transforms.ToTensor(),           # Convert images to tensors
+    transforms.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225])  # Normalize the image tensors
+])
+
+# Create train and test datasets
+train_dataset = ImageFolder(train_folder, transform=transform)
+test_dataset = ImageFolder(test_folder, transform=transform)
+
+# Create train and test data loaders
+train_loader = DataLoader(train_dataset, batch_size=32, shuffle=True)
+test_loader = DataLoader(test_dataset, batch_size=32, shuffle=False)
+
+# Load the pre-trained VGG model
+model = VGG.from_pretrained('vgg19', num_classes=102)  # Update num_classes to match your dataset
+
+# Define the loss function and optimizer
+criterion = torch.nn.CrossEntropyLoss()
+optimizer = torch.optim.SGD(model.parameters(), lr=0.001, momentum=0.9)
+
+# Training loop
+for epoch in range(10):  # Adjust the number of epochs as needed
+    model.train()
+    for images, labels in train_loader:
+        optimizer.zero_grad()
+        outputs = model(images)
+        loss = criterion(outputs, labels)
+        loss.backward()
+        optimizer.step()
+
+    # Evaluation on test set
+    model.eval()
+    test_loss = 0
+    correct = 0
+    with torch.no_grad():
+        for images, labels in test_loader:
+            outputs = model(images)
+            test_loss += criterion(outputs, labels).item()
+            _, predicted = torch.max(outputs, 1)
+            correct += (predicted == labels).sum().item()
+
+    test_loss /= len(test_loader.dataset)
+    accuracy = 100.0 * correct / len(test_loader.dataset)
+    print(f"Epoch: {epoch+1}/{num_epochs}, Test Loss: {test_loss:.4f}, Accuracy: {accuracy:.2f}%")
--- assignment3/yolo_v5.py
+++ assignment3/yolo_v5.py
@@ -20,3 +20,76 @@ results.pandas().xyxy[0]  # img1 predictions (pandas)
 # 1  433.50  433.50   517.5  714.5    0.687988     27     tie
 # 2  114.75  195.75  1095.0  708.0    0.624512      0  person
 # 3  986.00  304.00  1028.0  420.0    0.286865     27     tie
+
+
+import torch
+import torch.nn as nn
+import torchvision.transforms as transforms
+from torch.utils.data import DataLoader, Dataset
+from torchvision.datasets import ImageFolder
+from torch.autograd import Variable
+
+
+# Define the flower dataset class
+class FlowerDataset(Dataset):
+    def __init__(self, root_dir, transform=None):
+        self.dataset = ImageFolder(root_dir, transform=transform)
+
+    def __len__(self):
+        return len(self.dataset)
+
+    def __getitem__(self, idx):
+        return self.dataset[idx]
+
+
+# Load and transform the dataset
+root_dir = '/path/to/your/extracted/flowers/dataset'  # Replace with the path to the extracted dataset
+transform = transforms.Compose([
+    transforms.Resize((640, 640)),  # Resize the images to the desired input size
+    transforms.ToTensor(),
+    transforms.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225])  # Normalize the images
+])
+dataset = FlowerDataset(root_dir, transform=transform)
+
+# Create a dataloader
+dataloader = DataLoader(dataset, batch_size=1, shuffle=True)
+
+# Model
+model = torch.hub.load('ultralytics/yolov5', 'yolov5s', pretrained=True)
+
+# Freeze model weights
+for param in model.parameters():
+    param.requires_grad = False
+
+# Replace the last layer for classification
+num_classes = 102  # Number of flower classes
+in_features = model.model[-1].fc.in_features
+model.model[-1].fc = nn.Linear(in_features, num_classes)
+
+# Training
+device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+model.to(device)
+
+# Define the loss function and optimizer
+criterion = nn.CrossEntropyLoss()
+optimizer = torch.optim.Adam(model.parameters())
+
+# Training loop
+num_epochs = 10
+for epoch in range(num_epochs):
+    running_loss = 0.0
+    for images, labels in dataloader:
+        images = images.to(device)
+        labels = labels.to(device)
+
+        optimizer.zero_grad()
+
+        outputs = model(images)
+        loss = criterion(outputs, labels)
+        loss.backward()
+        optimizer.step()
+
+        running_loss += loss.item()
+
+    epoch_loss = running_loss / len(dataloader)
+    print(f"Epoch {epoch + 1}/{num_epochs}, Loss: {epoch_loss:.4f}")
