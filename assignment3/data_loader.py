7bc5ee841c9d1a1d59cb797b2bc51460630df5f3
Tomer Doitshman <>
2023-06-08 00:43:01 +0300

    ass1 proj first commit

--- assignment3/data_loader.py
+++ assignment3/data_loader.py
@@ -7,6 +7,25 @@ from torch.utils.data import DataLoader, Dataset
 import numpy as np
 
 
+class FlowerDataset(Dataset):
+    def __init__(self, root_dir, transform=None):
+        self.dataset = ImageFolder(root_dir, transform=transform)
+        self.labels = self.load_labels(root_dir)
+
+    def load_labels(self, root_dir):
+        labels_file = os.path.join(root_dir, 'imagelabels.mat')
+        labels = sio.loadmat(labels_file)['labels'][0]
+        return labels - 1  # Adjust labels to be zero-indexed
+
+    def __len__(self):
+        return len(self.dataset)
+
+    def __getitem__(self, idx):
+        image, _ = self.dataset[idx]  # Discard the original label
+        label = self.labels[idx]
+        return image, label
+
+
 class FlowerDataLoader:
     def __init__(self, root_dir, batch_size=100):
         self.root_dir = root_dir
@@ -16,9 +35,7 @@ class FlowerDataLoader:
             transforms.ToTensor(),
             transforms.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225])
         ])
-
-        self.dataset = ImageFolder(root_dir, transform=self.transform)
-        self.labels = self.load_labels()
+        self.dataset = FlowerDataset(root_dir=self.root_dir, transform=self.transform)
 
     def load_labels(self):
         labels_file = os.path.join(self.root_dir, 'imagelabels.mat')
@@ -53,8 +70,6 @@ class FlowerDataLoader:
 if __name__ == '__main__':
     # Example usage
     train_folder = '/Users/tomerdoitshman/Desktop/other/D_non_shared/ass3_dataset'
-    test_folder = '/Users/tomerdoitshman/Desktop/other/D_non_shared/ass3_dataset'
-
     flower_data_loader = FlowerDataLoader(train_folder, batch_size=100)
     train_loader = flower_data_loader.get_train_data_loader()
     validation_loader = flower_data_loader.get_validation_data_loader()
--- assignment3/yolo_v5.py
+++ assignment3/yolo_v5.py
@@ -1,84 +1,72 @@
-import os
 import torch
 import torch.nn as nn
-import torchvision.transforms as transforms
-from torch.utils.data import DataLoader, Dataset
-from torchvision.datasets import ImageFolder
-import scipy.io as sio
+from data_loader import FlowerDataLoader
 from sklearn.metrics import accuracy_score, precision_score
 
 
-# Define the flower dataset class
-class FlowerDataset(Dataset):
-    def __init__(self, root_dir, transform=None):
-        self.dataset = ImageFolder(root_dir, transform=transform)
-        self.labels = self.load_labels(root_dir)
+class YoloV5:
+    def __init__(self):
+        self.train_folder = '/Users/tomerdoitshman/Desktop/other/D_non_shared/ass3_dataset'
+        self.flowers_data_loader = FlowerDataLoader(root_dir=self.train_folder, batch_size=100)
+        self.dataloader = None
+        self.model = None
 
-    def load_labels(self, root_dir):
-        labels_file = os.path.join(root_dir, 'imagelabels.mat')
-        labels = sio.loadmat(labels_file)['labels'][0]
-        return labels - 1  # Adjust labels to be zero-indexed
+    def load_data(self):
+        try:
+            self.dataloader = self.flowers_data_loader.get_train_data_loader()
+        except Exception as e:
+            print('Exception %s occurred during load_data.' % e)
 
-    def __len__(self):
-        return len(self.dataset)
+    def load_model(self):
+        try:
+            # Load the pre-trained ResNet model
+            self.model = torch.hub.load('pytorch/vision:v0.9.0', 'resnet18', pretrained=True)
+        except Exception as e:
+            print('Exception %s occurred during load_model.' % e)
 
-    def __getitem__(self, idx):
-        image, _ = self.dataset[idx]  # Discard the original label
-        label = self.labels[idx]
-        return image, label
+    def train_model(self):
+        try:
+            # Replace the last linear layer for classification
+            num_classes = 102  # Number of flower classes
+            in_features = self.model.fc.in_features
+            self.model.fc = nn.Linear(in_features, num_classes)
+            # Training
+            device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+            self.model.to(device)
+            # Define the loss function and optimizer
+            criterion = nn.CrossEntropyLoss()
+            optimizer = torch.optim.Adam(self.model.parameters())
+            # Training loop
+            num_epochs = 1
+            for epoch in range(num_epochs):
+                running_loss = 0.0
+                batch_idx = 0
+                for images, labels in self.dataloader:
+                    print('Running on batch %d' % batch_idx)
+                    batch_idx += 1
+                    images = images.to(device)
+                    labels = labels.to(device)
+                    optimizer.zero_grad()
+                    outputs = self.model(images)
+                    loss = criterion(outputs, labels)
+                    loss.backward()
+                    optimizer.step()
+                    running_loss += loss.item()
+                    _, predicted = torch.max(outputs, 1)
+                    predicted = predicted.cpu().numpy()
+                    labels = labels.cpu().numpy()
+                    accuracy = accuracy_score(labels, predicted)
+                    precision = precision_score(labels, predicted, average='weighted')
+                    print('Accuracy:', accuracy)
+                    print('Precision:', precision)
+                epoch_loss = running_loss / len(self.dataloader)
+                print(f"Epoch {epoch + 1}/{num_epochs}, Loss: {epoch_loss:.4f}")
+        except Exception as e:
+            print('Exception %s occurred during train_model.' % e)
 
 
-# Load and transform the dataset
-root_dir = '/Users/tomerdoitshman/Desktop/other/D_non_shared/ass3_dataset'  # Replace with the
-# path to the extracted dataset
-transform = transforms.Compose([
-    transforms.Resize((224, 224)),  # Resize the images to the desired input size for the ResNet model
-    transforms.ToTensor(),
-    transforms.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225])  # Normalize the images
-])
-dataset = FlowerDataset(root_dir, transform=transform)
-
-# Create a dataloader
-dataloader = DataLoader(dataset, batch_size=100, shuffle=True)
-
-# Load the pre-trained ResNet model
-model = torch.hub.load('pytorch/vision:v0.9.0', 'resnet18', pretrained=True)
-
-# Replace the last linear layer for classification
-num_classes = 102  # Number of flower classes
-in_features = model.fc.in_features
-model.fc = nn.Linear(in_features, num_classes)
-
-# Training
-device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
-model.to(device)
-
-# Define the loss function and optimizer
-criterion = nn.CrossEntropyLoss()
-optimizer = torch.optim.Adam(model.parameters())
-
-# Training loop
-num_epochs = 1
-for epoch in range(num_epochs):
-    running_loss = 0.0
-    batch_idx = 0
-    for images, labels in dataloader:
-        print('Running on batch %d' % batch_idx)
-        batch_idx += 1
-        images = images.to(device)
-        labels = labels.to(device)
-        optimizer.zero_grad()
-        outputs = model(images)
-        loss = criterion(outputs, labels)
-        loss.backward()
-        optimizer.step()
-        running_loss += loss.item()
-        _, predicted = torch.max(outputs, 1)
-        predicted = predicted.cpu().numpy()
-        labels = labels.cpu().numpy()
-        accuracy = accuracy_score(labels, predicted)
-        precision = precision_score(labels, predicted, average='weighted')
-        print('Accuracy:', accuracy)
-        print('Precision:', precision)
-    epoch_loss = running_loss / len(dataloader)
-    print(f"Epoch {epoch + 1}/{num_epochs}, Loss: {epoch_loss:.4f}")
+if __name__ == '__main__':
+    yolo_v5 = YoloV5()
+    yolo_v5.load_data()
+    yolo_v5.load_model()
+    yolo_v5.train_model()
